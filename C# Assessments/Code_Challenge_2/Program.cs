74b1a799bb9b0b87332dc55e1b54b9e17af80c99
Adikesava4567 <adikesava.thummala@infinite.com>
2024-06-19 11:37:51 +0530

    Add files via upload
    assessments pushed local folder

---
+++ C# Assessments/Code_Challenge_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the string: ");
+            string inputString = Console.ReadLine();
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                string newString = ExchangeFirstAndLastCharacters(inputString);
+                Console.WriteLine($"New string : {newString}");
+            }
+            else
+            {
+                Console.WriteLine("Input string is empty");
+            }
+            Console.Read();
+        }
+        static string ExchangeFirstAndLastCharacters(string input)
+        {
+            if (input.Length < 2)
+            {
+                return input;
+            }
+
+            char firstChar = input[0];
+            char lastChar = input[input.Length - 1];
+
+            string newstring = lastChar + input.Substring(1, input.Length - 2) + firstChar;
+            return newstring;
+        }
+    }
+}
---
+++ C# Assessments/Code_Challenge_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            FindLargestNumber(num1, num2, num3);
+        }
+
+        static void FindLargestNumber(int num1,int num2, int num3)
+        {
+            int Largest = num1;
+            if (num2 > Largest)
+            {
+                Largest = num2;
+            }
+            if (num3 > Largest)
+            {
+                Largest = num3;
+            }
+
+            Console.WriteLine($"The largest Number is :{Largest}");
+            Console.ReadLine();
+        }
+    }
+}
