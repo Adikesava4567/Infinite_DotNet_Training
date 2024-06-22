5dd8e214be31d7aad7e85cb32d41325b792c2162
Adikesava4567 <adikesava.thummala@infinite.com>
2024-06-22 21:29:11 +0530

    Add files via upload

---
+++ Csharp_Practice_programs/DotNet_Demos/Demo_02/Demo_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello All,Welcome to .Net Training");
+            Console.Read();
+        }
+    }
+}
