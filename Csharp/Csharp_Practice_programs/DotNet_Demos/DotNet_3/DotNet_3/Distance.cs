7883902de92191b6e4488f7051ad864d1f061de8
Adikesava4567 <adikesava.thummala@infinite.com>
2024-06-22 21:40:53 +0530

    Add files via upload

---
+++ Csharp/Csharp_Practice_programs/DotNet_Demos/DotNet_3/DotNet_3/Distance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_3
+{
+    class Distance
+    {
+        public int dist1;
+        public Distance(int d)
+        {
+            dist1=d;
+        }
+
+        //overloading ++
+        public static Distance operator ++(Distance dis1)
+        {
+            Distance temp = new Distance(1);
+            temp.dist1 = dis1.dist1 + 1;
+            return temp;
+
+
+        }
+
+
+  
+
+        static void Main(string[] args)
+        {
+            Distance d1 = new Distance(5);
+            d1++;
+            Console.WriteLine(d1.dist1);
+            Console.Read();
+            
+        
+        }
+
+
+    }
+
+}
---
+++ Csharp/Csharp_Practice_programs/DotNet_Demos/DotNet_3/DotNet_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_3
+{
+    public class Program
+    {
+        static int data1;
+        string str;
+        public float fvar = 22.5f;
+        public static string s = "hello all";
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(data1 + " ",p.str + " ",p.fvar + " " + s);
+            Console.Read();
+        }
+    }
+}
